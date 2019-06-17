2e63b3b20fa87608a79eda5239892498c2ae152d
prajat <rajat.singh1084@gmail.com>
2019-06-17 15:39:28 +0530

    sounds added

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 console.log("hey rajat");
 
 
+let numberofdrums = document.querySelectorAll(".drum").length;
 
-let numberofdrus=document.querySelectorAll(".drum").length;
 
-for(let i=0;i<numberofdrus;i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click",()=>{
-        let audio=new Audio("sounds/tom-1.mp3");
-        audio.play();
-        
+for (let i = 0; i < numberofdrums; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        var buttontext = this.innerHTML;
+        switch (buttontext) {
+            case "w":
+                let tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "a":
+                let tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "s":
+                let tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "d":
+                let tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case "j":
+                let kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+            case "k":
+                let snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+
+            case "l":
+                let crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+
+            default:
+                console.log(buttontext);
+
+
+
+
+        }
+
+
     });
 
 }
+
 
 
 // function handleclick(){
@@ -21,3 +62,5 @@ for(let i=0;i<numberofdrus;i++){
 // console.log("hey rajat");
 // },2000);
 
+// let audio=new Audio("sounds/tom-1.mp3");
+//         audio.play();
