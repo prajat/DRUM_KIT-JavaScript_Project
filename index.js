6d58cbac397c3859a71b4916fd9968442f2364df
prajat <rajat.singh1084@gmail.com>
2019-06-17 14:50:07 +0530

    imaged added

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ let numberofdrus=document.querySelectorAll(".drum").length;
 
 for(let i=0;i<numberofdrus;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",()=>{
-        alert("button clicked");
+        let audio=new Audio("sounds/tom-1.mp3");
+        audio.play();
+        
     });
 
 }
